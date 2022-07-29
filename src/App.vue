d209c7715bd367f13dd3cc15c9ba07b4f7817a67
Lare Fadhil <larefazl2244@gmail.com>
2022-07-30 01:14:21 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,97 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <gmap-map
+      :center="center"
+      :zoom="18"
+      map-style-id="roadmap"
+      :options="mapOptions"
+      style="width: 100vmin; height: 50vmin"
+      ref="mapRef"
+      @click="handleMapClick"
+    >
+      <GmapMarker
+        :position="marker.position"
+        :clickable="true"
+        :draggable="true"
+        @drag="handleMarkerDrag"
+        @click="panToMarker"
+      />
+    </gmap-map>
+    <button @click="geolocate">Detect Location</button>
+
+    <p>Selected Position: {{ marker.position }}</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      marker: { position: { lat: 10, lng: 10 } },
+      center: { lat: 10, lng: 10 },
+      mapOptions: {
+        disableDefaultUI: true,
+      },
+    };
+  },
+  mounted() {
+    this.geolocate();
+    
+  },
+  methods: {
+    fetchData() {
+      if (!("geolocation" in navigator)) {
+        this.errorStr = "Geolocation is not available.";
+        return;
+      }
+      this.gettingLocation = true;
+      navigator.geolocation.watchPosition(
+        pos => {
+          this.gettingLocation = false;
+          this.initialPosition.lat = pos.coords.latitude;
+          this.initialPosition.lng = pos.coords.longitude;
+          const userData = {
+            position: {
+              lat: pos.coords.latitude,
+              lng: pos.coords.longitude
+            },
+            userName: this.usersName
+          };
+          this.userlocation = userData;
+          this.updateRoom(userData);
+        },
+        err => {
+          this.gettingLocation = false;
+          this.errorStr = err.message;
+        }
+      );
+    },
+    //detects location from browser
+    geolocate() {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.marker.position = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+        this.panToMarker();
+      });
+    },
+    //sets the position of marker when dragged
+    handleMarkerDrag(e) {
+      this.marker.position = { lat: e.latLng.lat(), lng: e.latLng.lng() };
+    },
+    //Moves the map view port to marker
+    panToMarker() {
+      this.$refs.mapRef.panTo(this.marker.position);
+      this.$refs.mapRef.setZoom(18);
+    },
+    //Moves the marker to click position on the map
+    handleMapClick(e) {
+      this.marker.position = { lat: e.latLng.lat(), lng: e.latLng.lng() };
+      console.log(e);
+    },
+  },
+  
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
 
-Vue.config.productionTip = false
+import Vue from "vue";
+import App from "./App.vue";
+
+Vue.config.productionTip = false;
+
+import * as VueGoogleMaps from "vue2-google-maps";
+
+Vue.use(VueGoogleMaps, {
+    load: {
+        key: "AIzaSyAwz5fzarj9DeWCVyWffcCO2V4c4p1bXsU",
+    },
+    installComponents: true,
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    render: (h) => h(App),
+}).$mount("#app");
